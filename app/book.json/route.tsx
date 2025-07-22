c8d47233b909c9942ff09bdff7eb48d339494e3e
Forest Ye <ezhou10@outlook.com>
2025-07-22 12:29:09 +0800

    四大名著

--- app/book.json/route.tsx
+++ app/book.json/route.tsx
@@ -1,5 +1,10 @@
 export const dynamic = "force-static"
 
 export async function GET() {
-  return Response.json({ name: 'Lee' })
+  return Response.json([
+    { name: '三国演义' },
+    { name: '水浒传' },
+    { name: '西游记' },
+    { name: '红楼梦' },
+  ])
 }
