908315535c3f5e7bdcbfbaab159d782f8bfcf1c5
Forest Ye <ezhou10@outlook.com>
2025-07-22 20:51:52 +0800

    尝试修复

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,10 @@ const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
   webpack: (config) => {
-    config.module.noParse = /alasql\.fs\.js$/;
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      'react-native-fs': false,
-      'react-native-fetch-blob': false
-    };
+    config.module.resolveAlias = {
+      ...config.module.resolveAlias,
+      'alasql': 'alasql/dist/alasql.min.js'
+    }
     return config;
   },
   turbopack: {
