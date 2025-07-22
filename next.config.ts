6b0f634eed39e4518bb0b121b893fcf65014530e
Forest Ye <ezhou10@outlook.com>
2025-07-22 21:45:15 +0800

    测试修复

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,13 @@ const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
   webpack: (config) => {
-    config.module.noParse = /alasql/;
+    config.module.rules.push({
+      resolve: {
+        alias: {
+          'alasql': 'alasql/dist/alasql.min.js'
+        },
+      }
+    });
     return config;
   },
   turbopack: {
