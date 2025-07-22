98e244d49c635927d588350471a2a9026678ef1f
Forest Ye <ezhou10@outlook.com>
2025-07-22 12:37:56 +0800

    更新主页

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
-      <div>Next.js on GitHub Pages</div>
-      <div>Hello World</div>
+      <ol>
+        <li>
+          <Link href="/book">Book</Link>
+        </li>
+        <li>
+          <Link href="/book.json">Book JSON</Link>
+        </li>
+        <li>
+          <Link href="/movie.json">Movie JSON</Link>
+        </li>
+      </ol>
     </main>
   );
 }
