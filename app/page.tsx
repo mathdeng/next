add7fa2674ab1fc0eef906268b20ed59e84daf63
Forest Ye <ezhou10@outlook.com>
2025-07-23 16:19:41 +0800

    主页更新

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,7 @@ export default function Home() {
           <Link className="text-blue-500" href="/yellow">黄页</Link>
         </li>
         <li>
-          <Link className="text-blue-500" href="/book">图书</Link>
-        </li>
-        <li>
-          <Link className="text-blue-500" href="/movie">影视</Link>
+          <Link className="text-blue-500" href="/qidian">起点</Link>
         </li>
         <li>
           <Link className="text-blue-500" href="/dt">日期时间</Link>
