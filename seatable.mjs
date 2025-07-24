a4120627571380bf55c03467df78b00421066fb3
Forest Ye <ezhou10@outlook.com>
2025-07-24 17:38:30 +0800

    测试 seatable

--- seatable.mjs
+++ seatable.mjs
@@ -6,7 +6,7 @@ const config = {
   APIToken: process.env.APITOKEN,
 };
 
-console.log('长度为：', process.env.ABC.length);
+console.log('长度为：', process.env.A.length);
 
 const base = new Base(config);
 await base.auth();
