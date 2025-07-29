92893f6b84cf21016d414685815f6ff0b498a532
Forest Ye <ezhou10@outlook.com>
2025-07-30 00:37:20 +0800

    加上 起点目录年月 计算页

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ export default function Home() {
           <Link className="text-blue-500" href="/qidian">起点</Link>
         </li>
         <li>
+          <Link className="text-blue-500" href="/qidian-catalog-month">起点 目录 年月</Link>
+        </li>
+        <li>
           <Link className="text-blue-500" href="/colon">冒号</Link>
         </li>
       </ol>
---
+++ app/qidian-catalog-month/table.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Table } from '@arco-design/web-react';
+
+type Record = {
+  dt: string;
+  cnt: number;
+}
+
+export default function TablePage({ data }: { data: Record[] }) {
+  const columns = [
+    {
+      title: '年月',
+      dataIndex: 'dt',
+      width: 100,
+    },
+    {
+      title: '字数',
+      dataIndex: 'cnt',
+      width: 100,
+      align: 'right' as const,
+    },
+  ];
+  const columnsWidth =  columns.reduce(
+    (acc, value) => acc + value.width, 0);
+
+  const Row = Table.Summary.Row;
+  const Cell = Table.Summary.Cell;
+  function summary(data: Record[] | undefined) {
+    return (
+      <Row>
+        <Cell>Total</Cell>
+        <Cell style={{textAlign: 'right'}}>
+          {(data !== undefined) && data.reduce((acc, value) => acc + value.cnt, 0)}
+        </Cell>
+      </Row>
+    );
+  }
+
+  return (
+    <Table
+      columns={columns}
+      scroll={{x: columnsWidth}}
+      data={data}
+      pagination={false}
+      summary={summary}
+    />
+  )
+}
