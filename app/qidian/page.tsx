32dc1e6e8a206f74b66f11c0f7f51c7df21f6417
Forest Ye <ezhou10@outlook.com>
2025-07-29 23:02:41 +0800

    将外部链接的颜色改为紫色

--- app/qidian/page.tsx
+++ app/qidian/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
       <ol className="list-decimal list-inside">
         {filteredData.map((record: Record) => (
           <li key={record.id}>
-            <a target="_blank" rel="noopener noreferrer" className="text-blue-500" href={'https://m.qidian.com/book/' + record.bookId.toString()}>
+            <a target="_blank" rel="noopener noreferrer" className="text-purple-500" href={'https://m.qidian.com/book/' + record.bookId.toString()}>
               {record.bookName}
             </a>
           </li>
