eea8a135cabe39e2c750baaab012fc6e3c0f1bd0
Forest Ye <ezhou10@outlook.com>
2025-08-07 00:05:54 +0800

    主页更新，加上日志

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,9 @@ export default function Home() {
         <li>
           <Link className="text-blue-500" href="/qidian-catalog-month">起点 目录 年月</Link>
         </li>
-
+        <li>
+          <Link className="text-blue-500" href="/action">日志</Link>
+        </li>
         <li>
           <Link className="text-blue-500" href="/colon">冒号</Link>
         </li>
