8b8458b782fe267486bfd923dd8cf94a13b187e9
Forest Ye <ezhou10@outlook.com>
2025-08-09 16:31:06 +0800

    主页，数据和页面分离

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,14 @@
 import Link from 'next/link';
-
+import data from './data.json'
 export default function Home() {
   return (
     <main>
-      <ol>
-        <li>
-          <Link className="text-blue-500" href="/yellow">黄页</Link>
-        </li>
-        <li>
-          <Link className="text-blue-500" href="/qidian">起点</Link>
-        </li>
-        <li>
-          <Link className="text-blue-500" href="/qidian-catalog-design">起点 目录 排版</Link>
-        </li>
-        <li>
-          <Link className="text-blue-500" href="/qidian-catalog-hour">起点 目录 小时</Link>
-        </li>
-        <li>
-          <Link className="text-blue-500" href="/qidian-catalog-day">起点 目录 日期</Link>
-        </li>
-        <li>
-          <Link className="text-blue-500" href="/qidian-catalog-month">起点 目录 年月</Link>
-        </li>
-        <li>
-          <Link className="text-blue-500" href="/action">日志</Link>
-        </li>
-        <li>
-          <Link className="text-blue-500" href="/colon">冒号</Link>
-        </li>
+      <ol className="list-decimal list-inside">
+        {data.map((item, index) => (
+          <li key={index.toString()}>
+            <Link className="text-blue-500" href={item.url}>{item.name}</Link>
+          </li>
+        ))}
       </ol>
     </main>
   );
