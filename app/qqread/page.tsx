87cf4c0e2ef8bc6082ca78c8483d9fbafc0b84c5
Forest Ye <ezhou10@outlook.com>
2025-08-31 03:30:35 +0800

    样式控制，而非单个空格

--- app/qqread/page.tsx
+++ app/qqread/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
   const searchParams = useSearchParams();
   const search = searchParams.get('search') ?? '';
   const filteredData = alasql(
-    `select * from ? where bookName like ? or authorName like ?`, 
+    `select * from ? where bookName like ? or authorName like ?`,
     [data, `%${search}%`, `%${search}%`]
   ) as Record[];
 
@@ -29,13 +29,17 @@ export default function Page() {
       <ol className="list-decimal list-inside">
         {filteredData.map((record: Record) => (
           <li key={record.qqId}>
-            <a target="_blank" rel="noopener noreferrer" className="text-indigo-500" href={`https://book.qq.com/book-detail/${record.qqId}/`}>
-              {record.bookName}
-            </a> <a target="_blank" rel="noopener noreferrer" className="text-indigo-500" href={`https://ubook.reader.qq.com/book-comment/${record.qqId}`}>
-              评论
-            </a> <a target="_blank" rel="noopener noreferrer" className="text-indigo-500" href={`https://m.qidian.com/book/${record.qidianId}/`}>
-              起点
-            </a>
+            <span className="space-x-2">
+              <a target="_blank" rel="noopener noreferrer" className="text-indigo-500" href={`https://book.qq.com/book-detail/${record.qqId}/`}>
+                {record.bookName}
+              </a>
+              <a target="_blank" rel="noopener noreferrer" className="text-indigo-500" href={`https://ubook.reader.qq.com/book-comment/${record.qqId}`}>
+                评论
+              </a>
+              <a target="_blank" rel="noopener noreferrer" className="text-indigo-500" href={`https://m.qidian.com/book/${record.qidianId}/`}>
+                起点
+              </a>
+            </span>
           </li>
         ))}
 
