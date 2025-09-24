a26ce2098d67cc333a35e0ae53cc6c8f548c96f6
Forest Ye <ezhou10@outlook.com>
2025-09-24 13:11:57 +0800

    起点订阅价格 完善

--- app/qidian-subscription/page.tsx
+++ app/qidian-subscription/page.tsx
@@ -1,12 +1,12 @@
 export default function Page() {
-  const num = Array.from({ length: 80 }).map((_, i) => i);
+  const num = Array.from({ length: 25 }).map((_, i) => i);
   return <ol className="list-decimal list-inside">
     {num.map(i => {
-      const START = 2000;
-      const cnt = START + i * 25;
-      const price = Math.floor(cnt / 1000 * 4);
+      const START = 8;
+      const price = START + i;
+      const cnt = price / 4 * 1000;
       return <li key={i}>
-        {cnt} 字，{price} 分
+        文章 {cnt} 字，价格 {price} 分
       </li>
     })}
   </ol>
